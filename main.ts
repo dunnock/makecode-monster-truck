af11c1cf290d9bf640c850d3c094caafeb340dc7
Maxim Vorobjov <maxim.vorobjov@gmail.com>
2023-10-07 09:25:42 +0200

    v1

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
-basic.forever(function on_forever() {
-    
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Diamond)
 })
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_UP, function () {
+    wuKong.stopMotor(wuKong.MotorList.M2)
+    basic.showIcon(IconNames.Meh)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_2_UP, function () {
+    wuKong.stopMotor(wuKong.MotorList.M2)
+    basic.showIcon(IconNames.Silly)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Giraffe)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_2_DOWN, function () {
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, -20)
+    basic.showIcon(IconNames.Yes)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_DOWN, function () {
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, 50)
+    basic.showIcon(IconNames.Happy)
+})
+basic.showIcon(IconNames.Surprised)
