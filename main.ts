d0d1fd324c496607249dabfe6849a371e976bf19
Maxim Vorobjov <maxim.vorobjov@gmail.com>
2023-10-11 09:58:25 +0200

    Add steering

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_4_DOWN, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 290)
+})
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Diamond)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_B_DOWN, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 210)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_A_DOWN, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 210)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_UP, function () {
     wuKong.stopMotor(wuKong.MotorList.M2)
     basic.showIcon(IconNames.Meh)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_3_DOWN, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 130)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_2_UP, function () {
     wuKong.stopMotor(wuKong.MotorList.M2)
@@ -15,11 +27,12 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showIcon(IconNames.Giraffe)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_2_DOWN, function () {
-    wuKong.setMotorSpeed(wuKong.MotorList.M2, -20)
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, -100)
     basic.showIcon(IconNames.Yes)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_DOWN, function () {
-    wuKong.setMotorSpeed(wuKong.MotorList.M2, 50)
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, 100)
     basic.showIcon(IconNames.Happy)
 })
+let angle = 0
 basic.showIcon(IconNames.Surprised)
